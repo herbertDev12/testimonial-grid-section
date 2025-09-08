36b48ca9ce6cc7676feb74e73d13486e3f569ce1
herbertDev12 <herbertn740@gmail.com>
2025-09-08 00:55:40 -0500

    feat: make the cards looks like the design

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,157 @@
-import { useState } from 'react'
+
 import './App.css'
 import {
   Card,
-  CardAction,
   CardContent,
-  CardDescription,
-  CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card"
 
 function App() {
 
   return (
-    <>
-      <Card>
-        <CardHeader>
-          <div className='flex'>
-            <img
-              src='/image-daniel.jpg'
-            />
-            <p>Daniel Clifford Verified Graduate</p>
-          </div>
-        </CardHeader>
-        <CardContent>
-          <div>
-            <p>
-              I received a job offer mid-course, and the subjects 
-              I learned were current, if not more so, in the company 
-              I joined. I honestly feel I got every penny’s worth.
-            </p>
-          </div>
-          <p>Card Content</p>
-        </CardContent>
+    <div className='bg-gray-100'>
+      <div className='grid gap-6 '>
+        <Card className='bg-purple-600 text-white gap-4 '>
+          <CardHeader>
+            <div className='flex'>
+              <img
+                src='/image-daniel.jpg'
+                className='rounded-full'
+              />
+              <p>Daniel Clifford Verified Graduate</p>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <p className='font-bold text-xl'>
+                I received a job offer mid-course, and the subjects 
+                I learned were current, if not more so, in the company 
+                I joined. I honestly feel I got every penny’s worth.
+              </p>
+            </div>
+            <div className='mt-4'>
+              <p>“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a 
+                transition and have heard some people who had an amazing experience here. I signed up 
+                for the free intro course and found it incredibly fun! I enrolled shortly thereafter. 
+                The next 12 weeks was the best - and most grueling - time of my life. Since completing 
+                the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”
+              </p>
+            </div>
+          
+          </CardContent>
         
-      </Card>
-    </>
+        </Card>
+        <Card className='bg-gray-500 text-white gap-4 '>
+          <CardHeader>
+            <div className='flex'>
+              <img
+                src='/image-jonathan.jpg'
+                className='rounded-full'
+              />
+              <p>Jonathan Walters Verified Graduate</p>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <p className='font-bold text-xl'>
+                The team was very supportive and kept me motivated
+              </p>
+            </div>
+            <div className='mt-4'> 
+              <p>“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer 
+              for a big company. This was one of the best investments I’ve made in myself. ”
+              </p>
+            </div>
+            
+          </CardContent>
+        
+        </Card>
+        <Card>
+          <CardHeader>
+            <div className='flex'>
+              <img
+                src='/image-jeanette.jpg'
+                className='rounded-full'
+              />
+              <p>Jeanette Harmon Verified Graduate</p>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <p className='font-bold text-xl'>
+                An overall wonderful and rewarding experience
+              </p>
+            </div>
+            <div className='mt-4'>
+              <p>“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living 
+              while doing something I love. ”
+              </p>
+            </div>
+            
+          </CardContent>
+        
+        </Card>
+        <Card className='bg-gray-800 text-white'>
+          <CardHeader>
+            <div className='flex'>
+              <img
+                src='/image-patrick.jpg'
+                className='rounded-full'
+              />
+              <p>Patrick Abrams Verified Graduate</p>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <p className='font-bold text-xl'>
+                Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and 
+                learning from their experiences was easy.
+            </p>
+            </div>
+            <div className='mt-4'>
+              <p>“ The staff seem genuinely concerned about my progress which I find really refreshing. The program 
+              gave me the confidence necessary to be able to go out in the world and present myself as a capable 
+              junior developer. The standard is above the rest. You will get the personal attention you need from 
+              an incredible community of smart and amazing people. ”
+              </p>
+            </div>
+            
+          </CardContent>
+        
+        </Card>
+        <Card>
+          <CardHeader>
+            <div className='flex'>
+              <img
+                src='/image-kira.jpg'
+                className='rounded-full'
+              />
+              <p>Kira Whittle Verified Graduate</p>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <p className='font-bold text-xl'>
+                Such a life-changing experience. Highly recommended!
+              </p>
+            </div>
+            <div className='mt-4'>
+              <p>“ Before joining the bootcamp, I’ve never written a line of code. I needed some structure from 
+              professionals who can help me learn programming step by step. I was encouraged to enroll by a former 
+             student of theirs who can only say wonderful things about the program. The entire curriculum and staff 
+              did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team 
+              project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial 
+              could ever have. In fact, I’ve often referred to it during interviews as an example of my developent 
+              experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 
+              100% recommend! ”
+              </p>
+            </div>
+            
+          </CardContent>
+        
+        </Card>
+      </div>
+    </div>
   )
 }
 
